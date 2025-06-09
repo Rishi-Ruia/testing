d75b4e0b5f31e636cfc991d8ef2094976f5eac1d
Rishi <rishiru@Outlook.com>
2025-06-09 11:09:43 -0700

    need to still do abilities but starting to code

--- src/pokemon.py
+++ src/pokemon.py
@@ -1,15 +1,20 @@
 from typing import Dict
 import random
 import math
-def __init__(self, dex:int, level:int, name:str, hp:int, attack:int, defense:int, spatk:int, spdef:int, speed:int, ability:str):
-    self.name = name
-    self.dex = dex
-    self.hp = math.floor((2* hp +31 + random.randint(0,63))*level)/100+5
-    self.attack = math.floor((2*attack+random.randint(0,63))*level/100+5)
-    self.defense = math.floor((2*defense+random.randint(0,63))*level/100+5)
-    self.spatk = math.floor((2*spatk+random.randint(0,63))*level/100+5)
-    self.spdef = math.floor((2*spdef+random.randint(0,63))*level/100+5)
-    self.speed = math.floor((2*speed+random.randint(0,63))*level/100+5)
-    self.boosts = {"attack": 0, "defense":0, "spatk":0, "spdef":0, "speed" :0}
-    self.level = level
-    self.ability = ability 
+import pandas as pd
+class pokemon:
+    dfevo = pd.read_csv("assets/spreadsheets/evo.csv")
+    dfmons = pd.read_csv("assets/spreadsheets/pokemon data.csv")
+    dfexp = pd.read_csv("assets/spreadsheets/exp chart.csv")
+    def __init__(self, dex:int):
+        self.name = 
+        self.dex = dex
+        self.hp = math.floor((2* hp +31 + random.randint(0,63))*level)/100+5
+        self.attack = math.floor((2*attack+random.randint(0,63))*level/100+5)
+        self.defense = math.floor((2*defense+random.randint(0,63))*level/100+5)
+        self.spatk = math.floor((2*spatk+random.randint(0,63))*level/100+5)
+        self.spdef = math.floor((2*spdef+random.randint(0,63))*level/100+5)
+        self.speed = math.floor((2*speed+random.randint(0,63))*level/100+5)
+        self.boosts = {"attack": 0, "defense":0, "spatk":0, "spdef":0, "speed" :0}
+        self.level = level
+        self.ability = ability 
