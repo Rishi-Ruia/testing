2cd93de2c06331c9d5efe0abb12900534e76c78c
raeedrahmanlwsd <1042297@lwsd.org>
2025-06-10 09:02:17 -0700

    Finished(?) Bot class and updated Player class.

--- src/bot.py
+++ src/bot.py
@@ -2,7 +2,25 @@ from typing import List
 from pokemon import Pokemon, Move
 from player import Player
 from random import randint
+import game
 
 class Bot(Player):
     def __init__(self) -> None:
         super().__init__()
+        
+    # Makes the bot do its move and returns a summary of the move. Also makes the bot switch if its current pokemon has fainted.
+    def bot_turn(self, player_mon: Pokemon, move: Move) -> str:
+        move_result: str = game.attack(self.current, move, player_mon, move.is_special)
+        if "healed" in move_result:
+            return f"{self.current.name} healed!"
+        if self.current.hp == 0:
+            # Disable the bot's moves
+            pass
+
+        return move_result
+    
+    # Forces the bot to switch in the event that its current pokemon faints or there is an ability that forces the bot to switch.
+    def auto_switch(self):
+        for i in range(len(self.mons)):
+            if (self.mons[i] != self.current) and (self.mons[i].hp != 0):
+                self.switch(i)
--- src/player.py
+++ src/player.py
@@ -4,23 +4,23 @@ from random import randint
 
 class Player:
     def __init__(self) -> None:
-        self.player_mons: List = []
-        self.current: Pokemon = self.player_mons[0]
+        self.mons: List = []
+        self.current: Pokemon = self.mons[0]
     
     # Switches the current Pokemon the player is using for the pokemon at index i and returns a string that summarizes the switch
     def switch(self, i: int) -> str:
-        if self.player_mons[i].hp == 0:
+        if self.mons[i].hp == 0:
             return None
-        if self.player_mons[i] == self.current:
+        if self.mons[i] == self.current:
             return None
         
         prev_name = self.current.name
-        self.current = self.player_mons[i]
+        self.current = self.mons[i]
         return f"{prev_name} switched out and {self.current.name} switched in!"
     
     # Checks if the player has lost
     def has_lost(self) -> bool:
-        for pokemon in self.player_mons:
+        for pokemon in self.mons:
             if pokemon.hp != 0:
                 return False
         
@@ -28,4 +28,4 @@ class Player:
     
     # Gets pokemon at index i
     def get_pokemon(self, i: int) -> Pokemon:
-        return self.player_mons[i]
+        return self.mons[i]
