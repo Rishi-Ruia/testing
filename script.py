1689bea9415edf8fa318829aa3b14d07dfe61401
Rishi Ruia <rishiru@outlook.com>
2025-06-11 18:14:04 -0700

    spreadsheet done

--- script.py
+++ script.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import requests
+import csv
 # # 1) Load your spreadsheet
 # dfevo = pd.read_csv("assets/spreadsheets/evo.csv")
 # dfmons = pd.read_csv("assets/spreadsheets/pokemon data.csv")
@@ -13,19 +14,32 @@ import requests
 
 
 # 1) Load your spreadsheet
-dfevo = pd.read_csv("assets/spreadsheets/evo.csv")
-dfmons = pd.read_csv("assets/spreadsheets/pokemon data.csv")
-local_names = list(dfevo["Pokemon"].str.lower())
-other_names = list(dfmons["name"].str.lower().unique())
-right_num = list(dfmons["dex"].unique())
-wrong_num = list(dfevo["#"].unique())
-# 2) Fetch the full list of species via HTTP
-order = list()
-# 3) Compute the difference
-for i in local_names:
-    index = other_names.index(i)+1
-    order.append(index)
-print(order)
+
+df = pd.read_csv("n.csv")
+print("dad")
+# with open("n.csv", "w", encoding="utf-8") as f:
+#     for i in range(1010):
+#         line = (
+#             str(df["Dex"][i]) + "," +
+#             str(df["Pokemon"][i]) + "," +
+#             "\"" + str(df["Moves"][i]).replace("-", "") + "\"" + "," +
+#             "\"" + str(df["Levels"][i]) + "\""
+#         )
+#         f.write(line + "\n")
+
+# dfevo = pd.read_csv("assets/spreadsheets/evo.csv")
+# dfmons = pd.read_csv("assets/spreadsheets/pokemon data.csv")
+# local_names = list(dfevo["Pokemon"].str.lower())
+# other_names = list(dfmons["name"].str.lower().unique())
+# right_num = list(dfmons["dex"].unique())
+# wrong_num = list(dfevo["#"].unique())
+# # 2) Fetch the full list of species via HTTP
+# order = list()
+# # 3) Compute the difference
+# for i in local_names:
+#     index = other_names.index(i)+1
+#     order.append(index)
+# print(order)
 # mons = list( dfevo["Pokemon"].str.lower())
 # s = set()
 # for mon in mons:
